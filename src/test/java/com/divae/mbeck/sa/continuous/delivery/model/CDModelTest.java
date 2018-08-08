9d65e123eb564265ac0cbd9d692f73555c8851e6
Matthias Beck <matthias.beck@diva-e.com>
2018-08-08 19:57:31 +0200

    add junit test

---
+++ src/test/java/com/divae/mbeck/sa/continuous/delivery/model/CDModelTest.java
@@ -0,0 +1,19 @@
+package com.divae.mbeck.sa.continuous.delivery.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CDModelTest {
+
+    @Test
+    public void isMessageSet() {
+        String testMessage = "test message";
+
+        CDModel cdModel = CDModel.builder()
+                .message(testMessage)
+                .build();
+
+        Assert.assertEquals(testMessage, cdModel.getMessage());
+    }
+
+}
